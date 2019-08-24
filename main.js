4fe53fb3782cd5072f6a09e493bf352356bb3d9f
Yang Sun <ysun62@ucsc.edu>
2019-08-24 15:04:30 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+let newItem = document.getElementById("addForm");
+let items = document.getElementById("items");
+let filter = document.getElementById("filter");
+newItem.addEventListener("submit", addItem);
+items.addEventListener("click", deleteItem);
+filter.addEventListener("keyup", filterItem);
+
+function addItem(e) {
+  e.preventDefault();
+
+  // create newli without the btn
+  let newli = document.createElement("li");
+  newli.className = "list-group-item";
+
+  // append newItem.value to newLi
+  let value = document.getElementById("newItem").value;
+  newli.appendChild(document.createTextNode(value));
+
+  // create the btn for the newli
+  let btn = document.createElement("button");
+  btn.className = "btn btn-danger btn-sm float-right delete";
+  btn.appendChild(document.createTextNode("X"));
+
+  // append btn to newli
+  newli.appendChild(btn);
+
+  // append newli to items
+  items.appendChild(newli);
+}
+
+function deleteItem(e) {
+  if (e.target.classList.contains("delete")) {
+    if (confirm("Are you sure?")) e.target.parentElement.remove();
+    // console.log(e.target.parentElement);
+  }
+}
+
+function filterItem(e) {
+  let text = e.target.value.toLowerCase();
+  let itemList = items.getElementsByTagName("li");
+
+  Array.from(itemList).forEach(function(item) {
+    let itemName = item.firstChild.textContent.toLowerCase();
+    if (itemName.indexOf(text) !== -1) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+  // console.log(itemList);
+}
